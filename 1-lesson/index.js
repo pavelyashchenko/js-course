f84e8d30ac06f67ae9b9384e7826a739f609dd3b
Pavel Yashchenko <yashchenkops@gmail.com>
2022-08-02 19:48:56 +0300

    second part 1-st lesson

--- 1-lesson/index.js
+++ 1-lesson/index.js
@@ -1,4 +1,11 @@
+// 1
+// const name = prompt('What`s your name?');
 
-const name = prompt('What`s your name?');
+// alert(`Hello, ${name}! How are you?`)
 
-alert(`Hello, ${name}! How are you?`)
+
+// 2
+const firstNum = prompt('Enter first number');
+const secondNum = prompt('Enter second number');
+
+const summ = alert(+firstNum + +secondNum);
